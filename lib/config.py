78aaa400f45b82149cb6867f17928b1339c4004c
Mike Mackintosh <m@splug.io>
2014-06-13 10:47:44 -0400

    updating configuration defaults

--- lib/config.py
+++ lib/config.py
@@ -2,20 +2,43 @@
 """
 This is the config for Titan
 """
-import ConfigParser
-from os import path
+import ConfigParser, inspect
+from os.path import dirname,abspath,join,isfile
 from sys import argv,exit
 
+# Set the titan directory
+TITAN_DIR = dirname(dirname(abspath(inspect.getfile(inspect.currentframe()))))
+
+# Titan Configuration
 def TiConfig( config_file):
-    config = {}
-    if path.isfile(config_file):
+
+    # Default Configuration
+    config = {
+        'main': {
+            'debug': 'true', 
+            'datastore': join(TITAN_DIR,'dbs'),
+            'logstore': join(TITAN_DIR,'logs'),
+            'modulestore': join(TITAN_DIR, "modules"),
+            'reportstore': join(TITAN_DIR, "reports/")
+            }, 
+        'reporting': {
+            'type': 'syslog|http', 
+            'enabled': False, # True or False 
+            'target': 'http://localhost:9393/titan'
+            }, 
+        'watcher': {
+            'enabled': 'true'
+            }
+        }
+
+    if isfile(config_file):
         cp = ConfigParser.ConfigParser()
         cp.read(config_file)
         for section in cp.sections():
-            config[section] = {}
             for option in cp.options(section):
                 config[section][option] = cp.get(section, option)
     else:
         exit('Failed to load configuration file, please check permissions')
 
+    print config
     return config
