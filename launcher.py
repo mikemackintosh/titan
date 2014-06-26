eae4ce5f5bcf8ee917afeac125a7ed65f60f5657
Mike Mackintosh <m@splug.io>
2014-06-26 11:28:37 -0400

    omit config.json from autoloader

--- launcher.py
+++ launcher.py
@@ -65,7 +65,7 @@ MODULE_PACKS = [
 MODULES = []
 for path in MODULE_PACKS:
     for root,dirs,files in walk(path):
-        for f in (f for f in files if f not in [".gitkeep", "README", "schema.py"]):
+        for f in (f for f in files if f not in [".gitkeep", "README", "schema.py", "config.json"]):
             MODULES.append( join(root,f) )
 
 
